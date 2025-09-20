70e3a2f7c3b2c9e4e168184fc259de4f9a35aac3
fimgaaa <Feyzax3333@gmail.com>
2025-09-20 23:00:19 +0300

    düzenleme işlemi oldu

--- HarikaYemekTarifleri.Maui/HarikaYemekTarifleri.Maui/Pages/RecipeDetailPage.xaml.cs
+++ HarikaYemekTarifleri.Maui/HarikaYemekTarifleri.Maui/Pages/RecipeDetailPage.xaml.cs
@@ -6,64 +6,64 @@ namespace HarikaYemekTarifleri.Maui.Pages;
 public partial class RecipeDetailPage : ContentPage
 {
     private readonly RecipeDetailViewModel _vm;
-    private readonly ToolbarItem _editToolbarItem;
+    //private readonly ToolbarItem _editToolbarItem;
     public RecipeDetailPage(RecipeDetailViewModel vm)
     {
         InitializeComponent();
         _vm = vm;
         BindingContext = vm;
 
-        _editToolbarItem = new ToolbarItem
-        {
-            Text = "Düzenle",
-            BindingContext = vm,
-        };
-        _editToolbarItem.SetBinding(ToolbarItem.CommandProperty, nameof(RecipeDetailViewModel.EditCommand));
+    //    _editToolbarItem = new ToolbarItem
+    //    {
+    //        Text = "Düzenle",
+    //        BindingContext = vm,
+    //    };
+    //    _editToolbarItem.SetBinding(ToolbarItem.CommandProperty, nameof(RecipeDetailViewModel.EditCommand));
 
-        UpdateEditToolbarItem();
-    }
+    //    UpdateEditToolbarItem();
+    //}
 
-    protected override void OnAppearing()
-    {
-        base.OnAppearing();
-        _vm.PropertyChanged += OnViewModelPropertyChanged;
-        UpdateEditToolbarItem();
-    }
+    //protected override void OnAppearing()
+    //{
+    //    base.OnAppearing();
+    //    _vm.PropertyChanged += OnViewModelPropertyChanged;
+    //    UpdateEditToolbarItem();
+    //}
 
-    protected override void OnDisappearing()
-    {
-        _vm.PropertyChanged -= OnViewModelPropertyChanged;
-        base.OnDisappearing();
-    }
+    //protected override void OnDisappearing()
+    //{
+    //    _vm.PropertyChanged -= OnViewModelPropertyChanged;
+    //    base.OnDisappearing();
+    //}
 
-    private void OnViewModelPropertyChanged(object? sender, PropertyChangedEventArgs e)
-    {
-        if (string.IsNullOrEmpty(e.PropertyName) || e.PropertyName == nameof(RecipeDetailViewModel.IsOwner))
-        {
-            if (Dispatcher?.IsDispatchRequired ?? false)
-            {
-                Dispatcher.Dispatch(UpdateEditToolbarItem);
-            }
-            else
-            {
-                UpdateEditToolbarItem();
-            }
-        }
-    }
-    private void UpdateEditToolbarItem()
-    {
+    //private void OnViewModelPropertyChanged(object? sender, PropertyChangedEventArgs e)
+    //{
+    //    if (string.IsNullOrEmpty(e.PropertyName) || e.PropertyName == nameof(RecipeDetailViewModel.IsOwner))
+    //    {
+    //        if (Dispatcher?.IsDispatchRequired ?? false)
+    //        {
+    //            Dispatcher.Dispatch(UpdateEditToolbarItem);
+    //        }
+    //        else
+    //        {
+    //            UpdateEditToolbarItem();
+    //        }
+    //    }
+    //}
+    //private void UpdateEditToolbarItem()
+    //{
 
-        if (_vm.IsOwner)
-        {
-            if (!ToolbarItems.Contains(_editToolbarItem))
-            {
-                ToolbarItems.Add(_editToolbarItem);
-            }
-        }
-        else if (ToolbarItems.Contains(_editToolbarItem))
-        {
-            ToolbarItems.Remove(_editToolbarItem);
+    //    if (_vm.IsOwner)
+    //    {
+    //        if (!ToolbarItems.Contains(_editToolbarItem))
+    //        {
+    //            ToolbarItems.Add(_editToolbarItem);
+    //        }
+    //    }
+    //    else if (ToolbarItems.Contains(_editToolbarItem))
+    //    {
+    //        ToolbarItems.Remove(_editToolbarItem);
 
-        }
+    //    }
     }
 }
