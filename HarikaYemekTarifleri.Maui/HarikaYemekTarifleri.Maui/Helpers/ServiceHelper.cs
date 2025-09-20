c2d133918703dc861b8f4337e275b3f05a4710b5
fimgaaa <Feyzax3333@gmail.com>
2025-09-20 16:04:00 +0300

    yorum satırı

--- HarikaYemekTarifleri.Maui/HarikaYemekTarifleri.Maui/Helpers/ServiceHelper.cs
+++ HarikaYemekTarifleri.Maui/HarikaYemekTarifleri.Maui/Helpers/ServiceHelper.cs
@@ -1,7 +1,16 @@
-﻿namespace HarikaYemekTarifleri.Maui.Helpers;
+﻿using System.Runtime.ConstrainedExecution;
+
+namespace HarikaYemekTarifleri.Maui.Helpers;
 
 public static class ServiceHelper
 {
+    //ServiceHelper = global servis erişim noktası
+
+    //Initialize() = uygulama başlarken DI Container’ı kaydeder
+
+    //Get<T>() = kayıtlı servisi her yerden almanı sağlar
+
+    //Kullanım: sayfa açmak, ViewModel almak, DI’dan servis çözmek
     public static IServiceProvider Services { get; private set; } = default!;
     public static void Initialize(IServiceProvider services) => Services = services;
     public static T Get<T>() where T : notnull => Services.GetRequiredService<T>();
